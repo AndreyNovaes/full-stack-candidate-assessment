2e6c0cf44a42a573fb50aac1323eb50730962d22
AndreyNovaes <andreynovaespro@gmail.com>
2023-04-15 10:29:39 -0300

    feat: add meli spider working well with new parameters

--- scrapyProject/scrapyProject/spiders/meli_spider.py
+++ scrapyProject/scrapyProject/spiders/meli_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 import scrapyProject.items as items
+
 from ..Generate_urls import Generate_urls
 
 class MeliSpiderSpider(scrapy.Spider):
@@ -16,10 +17,24 @@ class MeliSpiderSpider(scrapy.Spider):
   start_urls = instancia_generate_urls.generate_urls_meli(categories, 2)
 
   def parse(self, response):
-    for product in response.xpath('//ol[@class="ui-search-layout ui-search-layout--stack"]'):
-      item = items.ScrapyprojectItem()
+    products = response.xpath('//li[contains(@class, "ui-search-layout__item")]')
+    category = response.url.split('/')[-1].split('_')[0]
 
-      self.logger.info(product)
-      item["description"] = product.css("h2::text").get()
+    for product in products:
+      item = items.ScrapyprojectItem()
+      
+      price_str = product.css("span.price-tag-amount > span.price-tag-fraction::text").get()
+      price_decimal = float(price_str.replace(".", "").replace(",", "."))
+      
+      item["image"] = product.css("img.ui-search-result-image__element::attr(data-src)").get()
+      item["price"] = price_decimal
+      item["description"] = product.css("h2.ui-search-item__title::text").get()
+      item["link"] = product.css("a.ui-search-link::attr(href)").get()
+      item["website"] = "Mercado Livre"
+      
+      if category == "celulares-smartphones":
+        item["category"] = "celular"
+      else:
+        item["category"] = category
 
       yield item
