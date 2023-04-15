5d62cef50a859bbb3768a33ba82ddd2a3ce21a29
AndreyNovaes <andreynovaespro@gmail.com>
2023-04-15 10:30:08 -0300

    feat: update buscape spider as it need to supply new parameters

--- scrapyProject/scrapyProject/spiders/buscape_spider.py
+++ scrapyProject/scrapyProject/spiders/buscape_spider.py
@@ -10,14 +10,19 @@ class BuscapeSpider(scrapy.Spider):
 
   def parse(self, response):
     products = response.css(".SearchCard_ProductCard_Inner__7JhKb")
+    category = response.url.split("/")[3].split("?")[0]
 
     for product in products:
       item = items.ScrapyprojectItem()
 
+      price_str = product.css("p[data-testid = 'product-card::price']::text").get()
+      price_decimal = float(price_str.replace("R$", "").replace(".", "").replace(",", "."))      
+
       item["description"] = product.css("h2::text").get()
-      item["price"] = product.css("p[data-testid = 'product-card::price']::text").get()
+      item["price"] = price_decimal
       item["image"] = product.css("img[src^='https']").attrib["src"]
       item["link"] = Generate_urls.mountUrl("https://www.buscape.com.br", product.css("a::attr(href)").get())
-      item["category"] = response.url.split("/")[3].split("?")[0]
+      item["category"] = category
+      item["website"] = "Buscape"
 
       yield item
